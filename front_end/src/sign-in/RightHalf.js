375db40b16af5c00947cd28abe1dac748ff7e9d3
nayabrasoolsb <nrasool030@gmail.com>
2022-11-29 00:48:34 +0530

    sign-in page is updated

---
+++ front_end/src/sign-in/RightHalf.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function RightHalf() {
+  return (
+    <>
+      <div>Sign in</div>
+      <div><input type="text" placeholder="Mobile/Phone"/></div>
+      <div><input type="text" placeholder="Password"/></div>
+    </>
+  );
+}
