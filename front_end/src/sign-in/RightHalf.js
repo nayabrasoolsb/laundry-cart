7176cf783435b6eca66ae8570f37cd9b86d5ad7b
nayabrasoolsb <nrasool030@gmail.com>
2022-11-29 18:27:55 +0530

    event handler are added

--- front_end/src/sign-in/RightHalf.js
+++ front_end/src/sign-in/RightHalf.js
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function RightHalf() {
+  const [data, setData] = useState({
+    mobile: "",
+    password: "",
+  });
+  function changeHandler(e) {
+    setData((oldData) => ({ ...oldData, [e.target.name]: e.target.value }));
+  }
+  function submitHandler(e) {
+    e.preventDefault();
+    console.log(data)
+  }
+  // console.log(data);
   return (
     <div id="right-half" className="child">
-      <div className="flex-children">
-        <div id="sign-in-text">Sign in</div>
-        <div>
-          <input type="text" placeholder="Mobile/Email" />
+      <form onSubmit={(e) => submitHandler(e)}>
+        <div className="flex-children">
+          <div id="sign-in-text">Sign in</div>
+          <div>
+            <input
+              value={data.mobile}
+              name="mobile"
+              type="text"
+              placeholder="Mobile/Email"
+              onChange={(e) => changeHandler(e)}
+            />
+          </div>
+          <div>
+            <input
+              value={data.password}
+              name="password"
+              type="text"
+              placeholder="Password"
+              onChange={(e) => changeHandler(e)}
+            />
+          </div>
+          <div id="password-reset">Forgot Password?</div>
         </div>
-        <div>
-          <input type="text" placeholder="Password" />
+
+        <div id="sign-in-btn">
+          <button type="submit">Sign In</button>
         </div>
-        <div id="password-reset">Forgot Password?</div>
-      </div>
-      <div id="sign-in-btn">
-        <div >Sign In</div>
-      </div>
+      </form>
     </div>
   );
 }
--- front_end/src/sign-up/RightHalf.js
+++ front_end/src/sign-up/RightHalf.js
@@ -1,51 +1,111 @@
-import { Link } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
 
 export default function RightHalf() {
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    state: "",
+    district: "",
+    address: "",
+    pincode: "",
+  });
+  function changeHandler(e) {
+    e.preventDefault();
+    setUserData((oldUserData) => ({
+      ...oldUserData,
+      [e.target.name]: e.target.value,
+    }));
+  }
+  console.log(userData);
   return (
     <div id="reg-right-half" className="child-reg">
       <div>
-        <div id="reg-content">
-          <div id="reg-title">REGISTER</div>
-          <div id="reg-grid">
-            <div>
-              <input placeholder="Name" type="text" />
-            </div>
-            <div>
-              <input placeholder="Email" type="text" />
-            </div>
-            <div>
-              <input placeholder="Phone" type="text" />
-            </div>
-            <div>
-              <input placeholder="State" type="text" />
-            </div>
-            <div>
-              <input placeholder="District" type="text" />
-            </div>
-            <div>
-              <input placeholder="Address" type="text" />
-            </div>
-            <div>
-              <input placeholder="Pincode" type="text" />
+        <form>
+          <div id="reg-content">
+            <div id="reg-title">REGISTER</div>
+            <div id="reg-grid">
+              <div>
+                <input
+                  value={userData.name}
+                  name="name"
+                  placeholder="Name"
+                  type="text"
+                  onChange={(e) => changeHandler(e)}
+                />
+              </div>
+              <div>
+                <input
+                  value={userData.email}
+                  name="email"
+                  placeholder="Email"
+                  type="text"
+                  onChange={(e) => changeHandler(e)}
+                />
+              </div>
+              <div>
+                <input
+                  value={userData.phone}
+                  name="phone"
+                  placeholder="Phone"
+                  type="text"
+                  onChange={(e) => changeHandler(e)}
+                />
+              </div>
+              <div>
+                <input
+                  value={userData.state}
+                  name="state"
+                  placeholder="State"
+                  type="text"
+                  onChange={(e) => changeHandler(e)}
+                />
+              </div>
+              <div>
+                <input
+                  value={userData.district}
+                  name="district"
+                  placeholder="District"
+                  type="text"
+                  onChange={(e) => changeHandler(e)}
+                />
+              </div>
+              <div>
+                <input
+                  value={userData.address}
+                  name="address"
+                  placeholder="Address"
+                  type="text"
+                  onChange={(e) => changeHandler(e)}
+                />
+              </div>
+              <div>
+                <input
+                  value={userData.pincode}
+                  name="pincode"
+                  placeholder="Pincode"
+                  type="text"
+                  onChange={(e) => changeHandler(e)}
+                />
+              </div>
             </div>
           </div>
-        </div>
 
-        <div id="reg-right-btm-flex">
-          <div id="terms">
-            <div>
-              <input type="checkbox" />
-              <label id="terms-text">
-                I agree to Terms & Condition receiving marketing and promotional
-                materials
-              </label>
+          <div id="reg-right-btm-flex">
+            <div id="terms">
+              <div>
+                <input type="checkbox" />
+                <label id="terms-text">
+                  I agree to Terms & Condition receiving marketing and
+                  promotional materials
+                </label>
+              </div>
+            </div>
+            <div id="reg-btn">
+              <button type="submit">Register</button>
             </div>
           </div>
-          <div id="reg-btn">
-            <Link>Register</Link>
-          </div>
-        </div>
+        </form>
       </div>
     </div>
   );
