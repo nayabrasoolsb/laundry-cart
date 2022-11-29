03edf5f680df007ae46c4f849b723610b291975d
nayabrasoolsb <nrasool030@gmail.com>
2022-11-29 14:29:10 +0530

    files inside sign-in folder are modified

--- front_end/src/sign-in/RightHalf.js
+++ front_end/src/sign-in/RightHalf.js
@@ -2,10 +2,20 @@ import React from "react";
 
 export default function RightHalf() {
   return (
-    <>
-      <div>Sign in</div>
-      <div><input type="text" placeholder="Mobile/Phone"/></div>
-      <div><input type="text" placeholder="Password"/></div>
-    </>
+    <div id="right-half" className="child">
+      <div className="flex-children">
+        <div id="sign-in-text">Sign in</div>
+        <div>
+          <input type="text" placeholder="Mobile/Email" />
+        </div>
+        <div>
+          <input type="text" placeholder="Password" />
+        </div>
+        <div id="password-reset">Forgot Password?</div>
+      </div>
+      <div id="sign-in-btn">
+        <div >Sign In</div>
+      </div>
+    </div>
   );
 }
--- front_end/src/sign-in/SignInPage.js
+++ front_end/src/sign-in/SignInPage.js
@@ -1,31 +1,19 @@
+import "../styles/sign-in.css";
+
 import Footer from "../components/Footer";
-import Info from "../components/Info";
-import { Link } from "react-router-dom";
+import LeftHalf from "./LeftHalf";
 import React from "react";
 import RightHalf from "./RightHalf";
-import SmallFooter from "../components/SmallFooter";
 
 export default function SignInPage() {
   return (
     <>
-    <div id="sign-in-main">
-      <div id="sign-in-row">
-        <div id="left-half" className="inline">
-          <div id="title-div">
-            <div id="title">Laundry Service</div>
-            <div id="title-about">Doorstep Wash & Dryclean Service</div>
-            <div id="account-creation">Don't Have An Account?</div>
-            <div id="reg-button"><Link to="/register">Register</Link></div>
-          </div>
-        </div>
-        <div id="right-half" className="inline">
-          <RightHalf />
-        </div>
+      <div id="parent">
+        <LeftHalf />
+
+        <RightHalf />
       </div>
-    </div>
-    <Footer />
-    <Info />
-    <SmallFooter />
+      <Footer />
     </>
   );
 }
