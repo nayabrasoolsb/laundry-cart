5fdaacf678df1e795494e32772f499992197625a
nayabrasoolsb <nrasool030@gmail.com>
2023-03-06 05:50:23 +0530

    some performance improve and sent data to database

--- front_end/src/components/CreateSummery.js
+++ front_end/src/components/CreateSummery.js
@@ -1,25 +1,84 @@
 import "../styles/create-summery.css";
 
-import React, { useContext, useRef } from "react";
+import React, { useContext, useRef, useState } from "react";
 
 import DataIterator from "./DataIterator";
-import { UserContext } from "../App";
+import { PriceContext } from "../App";
 
-export default function CreateSummery({ data, onCancel, prices }) {
+export default function CreateSummery({ data, response, onCancel }) {
   let subTotal = useRef(0);
-  const userData = useContext(UserContext);
+  const prices = useContext(PriceContext);
+
+  const [loading, setLoading] = useState(false);
+
+  function orderId() {
+    let orId = "OR";
+    orId += Math.ceil(Math.random() * 199_999_999) + 100_000_000;
+    return orId;
+  }
+
   const pickupCharges = 90;
+  const dataIterator = data.map((product, index) => {
+    let total = 0;
+    const { type, towel, bleach, quantity, ironing } = product;
+    if (towel) total += Number(prices[type]["towel"]);
+    if (bleach) total += Number(prices[type]["bleach"]);
+    if (ironing) total += Number(prices[type]["ironing"]);
+    if (product["washing-machine"])
+      total += Number(prices[type]["washing-machine"]);
+    subTotal.current += quantity * total;
+    return (
+      <DataIterator total={total} key={`key is ${index}`} product={product} />
+    );
+  });
+
+  const storeLocation = "Near Phone booth 10th road";
+  const city = "Bangalore";
+  const storePhone = "+91 9988776655";
   let totalItems = 0;
+  const totalPrice = parseInt(subTotal.current) + pickupCharges;
+
   data.forEach((product) => {
     const { quantity, ironing, towel, bleach } = product;
     if (product["washing-machine"] || towel || ironing || bleach) {
       totalItems += parseInt(quantity);
     }
   });
-  const totalPrice = subTotal.current + pickupCharges;
-  const phone = "+91 9988776655";
-  const city = "Bangalore";
-  const storeLocation = "Near Phone booth 10th road";
+
+  const userData = JSON.parse(localStorage.getItem("user"));
+  const options = {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: localStorage.getItem("token"),
+    },
+    body: JSON.stringify({
+      products: data,
+      storeLocation:"Jp Nagar",
+      city,
+      storePhone,
+      totalItems,
+      price: totalPrice,
+      orderId: orderId(),
+    }),
+  };
+  async function creatingOrder() {
+    setLoading(true);
+    await fetch("http://localhost:3004/api/v1/user/create", options)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if(data.status == "success"){
+          response()
+        }
+      })
+      .catch((err) => {
+        console.log("error is ", err);
+      });
+    setLoading(false);
+  }
+
   return (
     <div className="summery-main">
       <div className="content">
@@ -35,32 +94,16 @@ export default function CreateSummery({ data, onCancel, prices }) {
           </div>
           <div>
             <div className="location-text">Store Location:</div>
-            <div className="detail-text">Near Phone booth 10th road</div>
+            <div className="detail-text">{storeLocation} </div>
           </div>
           <div>
             <div className="location-text">Phone</div>
-            <div className="detail-text">+91 9988776655</div>
+            <div className="detail-text">{storePhone} </div>
           </div>
         </div>
         <div className="order-details">
           <div className="text">Order Details</div>
-          {data.map((product, index) => {
-            let total = 0;
-            const { type, towel, bleach, quantity, ironing } = product;
-            if (towel) total += Number(prices[type]["towel"]);
-            if (bleach) total += Number(prices[type]["bleach"]);
-            if (ironing) total += Number(prices[type]["ironing"]);
-            if (product["washing-machine"])
-              total += Number(prices[type]["washing-machine"]);
-            subTotal.current = subTotal.current + quantity * total;
-            return (
-              <DataIterator
-                total={total}
-                key={`key is ${index}`}
-                product={product}
-              />
-            );
-          })}
+          {dataIterator}
           <div className="sub-total">
             <span>Sub total:</span>
             <span> {subTotal.current}</span>
@@ -77,21 +120,26 @@ export default function CreateSummery({ data, onCancel, prices }) {
             </div>
           </div>
           <hr style={{ margin: "10px 0" }} />
-          <div>
+          <div style={{ marginBottom: "15px" }}>
             <div className="address-text">Address</div>
           </div>
           <div className="address">
             <h3>Home</h3>
             <div>
               {userData.address}, {userData.district}, {userData.state},{" "}
-              {userData.pincode}{" "}
+              {userData.pincode}
             </div>
           </div>
         </div>
-
-        <div className="confirm">
-          <button>Confirm</button>
-        </div>
+        {loading ? (
+          <div>loading...</div>
+        ) : (
+          <div className="confirm">
+            <button disabled={totalPrice === 90} onClick={creatingOrder}>
+              Confirm
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
